67d5f287f87b48d3021d8684a167041e500a9491
coffeeandpouch <sameinsidler@me.com>
2021-04-12 05:20:08 -0400

    commits all modules

--- server.js
+++ server.js
@@ -1,16 +1,59 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 
 const app = express();
 
-app.use(morgan("dev"));
+const morganSetting = process.env.NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganSetting));
 
-app.use((req, res) => {
-  res.send("Hello, world!");
+const validTypes = [
+  `Bug`,
+  `Dark`,
+  `Dragon`,
+  `Electric`,
+  `Fairy`,
+  `Fighting`,
+  `Fire`,
+  `Flying`,
+  `Ghost`,
+  `Grass`,
+  `Ground`,
+  `Ice`,
+  `Normal`,
+  `Poison`,
+  `Psychic`,
+  `Rock`,
+  `Steel`,
+  `Water`,
+];
+app.use(function validateBearerToken(req, res, next) {
+  // move to the next middleware
+  next();
 });
 
-const PORT = 8000;
+function handleGetTypes(req, res) {
+  res.json(validTypes);
+}
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.get("/types", handleGetTypes);
+
+function handleGetPokemon(req, res) {
+  res.send("hello, Pokemon!");
+}
+
+app.get("/pokemon", handleGetPokemon);
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.Node_ENV === "production") {
+    response = { error: { message: "Servr Error" } };
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 });
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {});
